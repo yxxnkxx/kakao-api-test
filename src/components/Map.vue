0887408ad072bd2e1cdd7f2bab918103d22c8edd
YunGyung Lee <yxxnkxx@gmail.com>
2022-12-18 21:54:31 +0900

    marker

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -19,6 +19,7 @@ export default {
       map: null,
       search: "",
       infowindow: null,
+      ps: null,
     };
   },
   mounted() {
@@ -43,10 +44,50 @@ export default {
         message += "경도는 " + latlng.getLng() + " 입니다";
 
         console.log(message);
-
-        this.map.ps = new kakao.maps.services.Places();
-        this.infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
       });
+      this.ps = new kakao.maps.services.Places();
+      this.infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+      this.ps.keywordSearch("이태원 맛집", placesSearchCB);
+      const map = this.map;
+      // 키워드 검색 완료 시 호출되는 콜백함수 입니다
+      function placesSearchCB(data, status) {
+        console.log("검색 완료");
+        if (status === kakao.maps.services.Status.OK) {
+          // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+          // LatLngBounds 객체에 좌표를 추가합니다
+          console.log(data);
+          var bounds = new kakao.maps.LatLngBounds();
+
+          for (var i = 0; i < data.length; i++) {
+            displayMarker(data[i]);
+            bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+          }
+
+          // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+          map.setBounds(bounds);
+        }
+      }
+
+      // 지도에 마커를 표시하는 함수입니다
+      function displayMarker(place) {
+        console.log("display marker");
+        // 마커를 생성하고 지도에 표시합니다
+        var marker = new kakao.maps.Marker({
+          map: map,
+          position: new kakao.maps.LatLng(place.y, place.x),
+        });
+
+        // 마커에 클릭이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, "click", function () {
+          // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+          this.infowindow.setContent(
+            '<div style="padding:5px;font-size:12px;">' +
+              place.place_name +
+              "</div>"
+          );
+          this.infowindow.open(this.map, marker);
+        });
+      }
     },
 
     addScript() {
@@ -54,45 +95,8 @@ export default {
 
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=e894c36415fadaef2e28d48b5d15ce93&libraries=services";
+        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=e894c36415fadaef2e28d48b5d15ce93&libraries=services,drawing";
       document.head.appendChild(script);
-    },
-    keySearch() {
-      this.map.ps.keywordSearch(this.search, this.placesSearchCB);
-    },
-    placesSearchCB(data, status) {
-      this.result = data;
-      if (status === kakao.maps.services.Status.OK) {
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        // LatLngBounds 객체에 좌표를 추가합니다
-        var bounds = new kakao.maps.LatLngBounds();
-
-        for (var i = 0; i < data.length; i++) {
-          this.displayMarker(data[i]);
-          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-        }
-
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-        this.map.setBounds(bounds);
-      }
-    },
-    displayMarker(place) {
-      // 마커를 생성하고 지도에 표시합니다
-      var marker = new kakao.maps.Marker({
-        map: this.map,
-        position: new kakao.maps.LatLng(place.y, place.x),
-      });
-
-      // 마커에 클릭이벤트를 등록합니다
-      kakao.maps.event.addListener(marker, "click", function () {
-        // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
-        this.infowindow.setContent(
-          '<div style="padding:5px;font-size:12px;">' +
-            place.place_name +
-            "</div>"
-        );
-        this.infowindow.open(this.map, marker);
-      });
     },
   },
 };
